3a230df4da852df921dbc72e243dca678ad15b68
Matthew Moore <mattholtmoore@gmail.com>
2021-11-20 22:33:44 -0500

    rethinking if statements

--- assets/script.js
+++ assets/script.js
@@ -107,27 +107,25 @@ function generatePassword() {
   var passwordLength = parseInt(
     prompt("How many characters would you like your password to have?"), 10
   );
-  
+
+  if (!passwordLength) {
+    alert("Please insert a number.")
+  }
+
   if (passwordLength >= 8 && passwordLength <= 128) {
     alert("You have selected to have " + passwordLength + " characters for your password length.");
-    console.log("User has selected to have " + passwordLength + " characters for password length.")
   }
 
   if (passwordLength < 8 || passwordLength > 128) {
     alert("Password length must be more than 7 characters and less than 129 characters. Please try again.");
-    console.log("User has selected to have " + passwordLength + " characters for password length. This is an invalid option.")
-    return null;
   }
 
   if (Number.isNaN(passwordLength)) {
     alert("A character other than a number was entered. Please choose a numeric character within provided range to continue.");
-    return null;
   }
-
 
   var personsChoice = [];
   var endResult = [];
-
 
   var addLowerCase = confirm("Would you like to include lowercase characters? Click OK to confirm.");
   if (addLowerCase) {
